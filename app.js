120b8be63ad7df87001c9e5bce432684fc75bddf
KasunAb <75435489+KasunAb@users.noreply.github.com>
2021-07-04 16:21:03 +0530

    Leval 4 enviromental variable

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require('express');
 const ejs = require('ejs');
 const port = 3000;
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+var md5 = require('md5');
+
 const app = express();
 
 app.use(express.static('public'));
@@ -20,8 +21,7 @@ const userSchema =new mongoose.Schema({
   password:String
 });
 
-const secret = process.env.SECRET;
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password']  });
+
 
 const User = new mongoose.model("User",userSchema);
 
@@ -44,7 +44,7 @@ app.get("/submit",(req,res)=>{
 app.post("/register",(req,res)=>{
   const newUser = new User({
     email:req.body.username,
-    password:req.body.password
+    password:md5(req.body.password)
   });
   newUser.save((err)=>{
     if(err)
@@ -56,7 +56,7 @@ app.post("/register",(req,res)=>{
 
 app.post("/login",(req,res)=>{
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
   User.findOne({email:username},(err,findUser)=>{
     if(err)
       console.log(err);
